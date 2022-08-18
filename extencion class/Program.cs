0dea6fd8b390df8e50b6013580197f0d515adf97
TunarN <tunarzn@code.edu.az>
2022-08-18 17:23:34 +0400

    extensionclass

---
+++ extencion class/Program.cs
@@ -0,0 +1,48 @@
+﻿using extencion_class.models;
+using System;
+
+namespace extencion_class
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Task1
+            Console.WriteLine(25.IsOdd());
+            Console.WriteLine(25.IsEven());
+            Console.WriteLine("Tunar".IsContainsDigit());
+            Console.WriteLine("tUnar".ToCapitalize());
+            int[] Arr = "Tunaara".GetValueIndexes('a');
+            foreach (int item in Arr)
+            {
+                Console.WriteLine(item);
+            }
+            #endregion
+            #region Task2
+            string fullname, groupNo;
+            int age;
+            do
+            {
+                Console.Write("Fullname: ");
+                fullname = Console.ReadLine();
+            } while (!student.CheckFullname(fullname));
+
+            do
+            {
+                Console.Write("Group No: ");
+                groupNo = Console.ReadLine();
+            } while (!student.CheckGroupNo(groupNo));
+
+
+            do
+            {
+                Console.Write("age: ");
+                age = Convert.ToInt32(Console.ReadLine());
+            } while (age <= 0 || age > 170);
+
+            student st = new student(fullname, groupNo, age);
+
+            #endregion
+        }
+    } 
+}
---
+++ extencion class/models/ExtentionMethods.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace extencion_class.models
+{
+   static class ExtentionMethods
+    {
+        public static bool IsOdd(this int num)
+        {
+            
+            if (num%2==1)
+            {
+                return true;
+            }
+            return false;
+        }
+        public static bool IsEven(this int num)
+        {
+            if (num%2==0)
+            {
+                return true;
+            }
+            return false;
+        }
+        public static bool IsContainsDigit(this string str)
+        {
+            foreach (var item in str)
+            {
+                if (item>47&&item<58)
+                {
+                    return true;
+                    
+                }
+               
+            }
+            return false;
+        }
+        public static string ToCapitalize(this string str)
+        {
+            StringBuilder s = new StringBuilder();
+            s.Append(Char.ToUpper(str[0]));
+            s.Append(str.Substring(1).ToLower());
+            return s.ToString();
+        }
+        public static int[] GetValueIndexes(this string str, char ch)
+        {
+            int[] Arr = new int[0];
+            for (int i = 0; i < str.Length; i++)
+            {
+                if (str[i] == ch)
+                {
+                    Array.Resize<int>(ref Arr, Arr.Length + 1);
+                    Arr[Arr.Length - 1] = i;
+                }
+            }
+
+            return Arr;
+        }
+
+    }
+}
